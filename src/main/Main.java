d1764ef9220ab00b270d096e69838619ac2fbbd4
Inho <churen1030@gmail.com>
2017-03-01 00:07:27 +0900

    FFT example added

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,27 @@
 package main;
 
+import main.Fft.*;
+
 public class Main {
 	public static void main(String[] args) {  
-       
-        System.out.println("Restart java"); 
-        System.out.println("Restart java"); 
+		int N = 8;//Integer.parseInt(args[0]);
+        Complex[] x = new Complex[N];
+
+        // original data
+        for (int i = 0; i < N; i++) 
+        {
+            x[i] = new Complex(i, 0);
+            x[i] = new Complex(-2 * Math.random() + 1, 0);
+        }
+        Fft.Complex.show(x, "x");
+
+        // FFT of original data
+        Complex[] y = Fft.Complex.fft(x);
+        Fft.Complex.show(y, "y = fft(x)");
+
+        // take inverse FFT
+        Complex[] z = Fft.Complex.ifft(y);
+        Fft.Complex.show(z, "z = ifft(y)");
           
     } 
 }
