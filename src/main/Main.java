74f56338cf2ab1ba7d59d85d5e07682d50054968
Inho <churen1030@gmail.com>
2017-02-28 23:49:52 +0900

    test2

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {  
        
         System.out.println("Restart java"); 
-        
+        System.out.println("Restart java"); 
+          
     } 
 }
